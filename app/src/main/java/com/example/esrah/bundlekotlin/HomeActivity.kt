b5e0dae6b47f21d19f86be41ac1683e0b296732c
esrah <esrahayta@hotmail.com>
2019-01-04 00:22:56 +0300

    initial commit

---
+++ app/src/main/java/com/example/esrah/bundlekotlin/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.esrah.bundlekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
