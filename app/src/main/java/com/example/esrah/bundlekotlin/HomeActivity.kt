10d66897422dba99fa30259209ef2c093d50b20c
esrah <esrahayta@hotmail.com>
2019-01-07 13:25:00 +0300

    todo

--- app/src/main/java/com/example/esrah/bundlekotlin/HomeActivity.kt
+++ app/src/main/java/com/example/esrah/bundlekotlin/HomeActivity.kt
@@ -2,19 +2,70 @@ package com.example.esrah.bundlekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import com.example.esrah.bundlekotlin.Profile.NotificationFragment
+import com.example.esrah.bundlekotlin.Profile.ProfilePagerAdapter
+import com.example.esrah.bundlekotlin.Profile.SavedArticlesFragment
 import com.example.esrah.bundlekotlin.Utils.BottomNavigationViewHelper
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    private val ACTIVITY_NO=0
+    private val TAG="HomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupNavigationView()
+        setupViewPager()
+
     }
 
     fun setupNavigationView(){
 
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView)
+        BottomNavigationViewHelper.setupNavigation(this,bottomNavigationView)
+        var menu=bottomNavigationView.menu
+        var menuItem=menu.getItem(ACTIVITY_NO)
+        menuItem.setChecked(true)
+
+    }
+    private fun setupViewPager() {
+        var tabAdlari = ArrayList<String>()
+        tabAdlari.add("Notification")
+        tabAdlari.add("Saved Articles")
+
+        var profilePagerAdapter = ProfilePagerAdapter(supportFragmentManager, tabAdlari)
+        profilePagerAdapter.addFragment(NotificationFragment())
+        profilePagerAdapter.addFragment(SavedArticlesFragment())
+
+        loginView.adapter=profilePagerAdapter
+
+
+        loginView.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+
+        })
+
+        tabs.setupWithViewPager(loginView)
+
+
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(0,0)
     }
 }
--- app/src/main/java/com/example/esrah/bundlekotlin/Utils/BottomNavigationViewHelper.kt
+++ app/src/main/java/com/example/esrah/bundlekotlin/Utils/BottomNavigationViewHelper.kt
@@ -1,5 +1,11 @@
 package com.example.esrah.bundlekotlin.Utils
 
+import android.content.Context
+import android.content.Intent
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import com.example.esrah.bundlekotlin.HomeActivity
+import com.example.esrah.bundlekotlin.R
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 
 
@@ -8,10 +14,37 @@ class BottomNavigationViewHelper {
     companion object {
 
         fun setupBottomNavigationView(bottomnavigationViewEx: BottomNavigationViewEx){
-            bottomnavigationViewEx.enableAnimation(false)
+
             bottomnavigationViewEx.setTextVisibility(false)
             bottomnavigationViewEx.enableShiftingMode(false)
 
+        }
+
+        fun setupNavigation(context: Context, bottomnavigationViewEx: BottomNavigationViewEx){
+
+            bottomnavigationViewEx.onNavigationItemSelectedListener=object :BottomNavigationView.OnNavigationItemSelectedListener{
+                override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+                    when(p0.itemId){
+
+                        R.id.home ->{
+
+                            val intent=Intent(context,HomeActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                            context.startActivity(intent)
+                            return true
+                        }
+
+                        R.id.profile ->{
+
+                            val intent=Intent(context,HomeActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                            context.startActivity(intent)
+                            return true
+                        }
+
+                    }
+                    return false
+                }
+            }
         }
     }
 
